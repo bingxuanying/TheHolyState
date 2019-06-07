8b69a68d680cb9a72c131877fdc0b291c6fa8049
arition <aritionkb@gmail.com>
2019-06-06 19:28:16 -0700

    selection

---
+++ Assets/Scripts/DragSelectionHandler.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+
+namespace Assets.Scripts
+{
+    internal class DragSelectionHandler : MonoBehaviour, IBeginDragHandler, IDragHandler, IEndDragHandler
+    {
+        public Image SelectionBoxImage;
+
+        private Vector2 _startPosition;
+        private Rect _selectionRect;
+
+        public void OnBeginDrag(PointerEventData eventData)
+        {
+            if (!(Input.GetKey(KeyCode.LeftControl) || Input.GetKey(KeyCode.RightControl) ||
+                  Input.GetKey(KeyCode.LeftShift) || Input.GetKey(KeyCode.RightShift)))
+                SelectableCharacterController.DeselectAll(new BaseEventData(EventSystem.current));
+            SelectionBoxImage.gameObject.SetActive(true);
+            _startPosition = eventData.position;
+            _selectionRect = new Rect();
+        }
+
+        public void OnDrag(PointerEventData eventData)
+        {
+            if (eventData.position.x < _startPosition.x)
+            {
+                _selectionRect.xMin = eventData.position.x;
+                _selectionRect.xMax = _startPosition.x;
+            }
+            else
+            {
+                _selectionRect.xMin = _startPosition.x; 
+                _selectionRect.xMax = eventData.position.x;
+            }
+
+            if (eventData.position.y < _startPosition.y)
+            {
+                _selectionRect.yMin = eventData.position.y;
+                _selectionRect.yMax = _startPosition.y;
+            }
+            else
+            {
+                _selectionRect.yMin = _startPosition.y;
+                _selectionRect.yMax = eventData.position.y;
+            }
+
+            SelectionBoxImage.rectTransform.offsetMin = _selectionRect.min;
+            SelectionBoxImage.rectTransform.offsetMax = _selectionRect.max;
+        }
+
+        public void OnEndDrag(PointerEventData eventData)
+        {
+            SelectionBoxImage.gameObject.SetActive(false);
+            foreach (var selectable in SelectableCharacterController.AllSelectable)
+            {
+                if (_selectionRect.Contains(Camera.main.WorldToScreenPoint(selectable.transform.position)))
+                {
+                    selectable.OnSelect(eventData);
+                }
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/SelectableCharacterController.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+namespace Assets.Scripts
+{
+    internal class SelectableCharacterController : MonoBehaviour, ISelectHandler, IPointerClickHandler, IDeselectHandler
+    {
+        public static HashSet<SelectableCharacterController> AllSelectable =
+            new HashSet<SelectableCharacterController>();
+
+        public static HashSet<SelectableCharacterController> CurrentSelected =
+            new HashSet<SelectableCharacterController>();
+
+        private SpriteRenderer _spriteRenderer;
+
+        public void OnDeselect(BaseEventData eventData)
+        {
+            CurrentSelected.Remove(this);
+            _spriteRenderer.color = Color.white;
+        }
+
+        public void OnPointerClick(PointerEventData eventData)
+        {
+            if (!(Input.GetKey(KeyCode.LeftControl) || Input.GetKey(KeyCode.RightControl) ||
+                  Input.GetKey(KeyCode.LeftShift) || Input.GetKey(KeyCode.RightShift)))
+                DeselectAll(eventData);
+            OnSelect(eventData);
+        }
+
+        public void OnSelect(BaseEventData eventData)
+        {
+            CurrentSelected.Add(this);
+            _spriteRenderer.color = Color.grey;
+        }
+
+        public static void DeselectAll(BaseEventData eventData)
+        {
+            foreach (var selectableCharacterController in CurrentSelected.ToList())
+                selectableCharacterController.OnDeselect(eventData);
+        }
+
+        private void Awake()
+        {
+            AllSelectable.Add(this);
+            _spriteRenderer = GetComponent<SpriteRenderer>();
+        }
+    }
+}
